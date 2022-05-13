1ca0cc0d7facd4150ce607e79114cd6c300c5112
Shenouda Fawzy <s.tawfeek@paytabs.com>
2022-05-13 19:55:24 +0200

    init repo

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	h := http.FileServer(http.Dir("assets"))
+
+	http.Handle("/", h)
+	http.HandleFunc("/hello", SayHello)
+	http.HandleFunc("/hook", hook)
+	fmt.Println("Should open the browser on https://localhost:1234")
+	log.Fatal(http.ListenAndServeTLS(":1234", "localhost.crt", "localhost.key", nil)) // Lets run it
+}
+
+func SayHello(w http.ResponseWriter, req *http.Request) {
+	log.Println("Hi my protocol is: ", req.Proto)
+	fmt.Fprintf(w, "Hi i'm the new protocol H2\n")
+}
+
+// hook will handle webhook calling
+func hook(w http.ResponseWriter, req *http.Request) {
+	log.Printf("%s method to %s\n", req.Method, req.URL)
+	err := req.ParseForm() // To application/x-www-form-urlencoded
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	fmt.Println(req.Form.Encode())
+	fmt.Println("--- Beautified ---")
+	fmt.Printf("%#v\n", req.Form)
+}
